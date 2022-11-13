2f7687877f7c7487844e659412592779f10b0bab
abdullahihearts <abdullahi40401.student@simad.edu.so>
2022-11-13 23:02:13 +0300

    practice

---
+++ src/javabgr/smallprogram.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javabgr;
+
+/**
+ *
+ * @author abdal
+ */
+public class smallprogram {
+    public static void main (String[]args){
+        /*int x=0;
+        int y=80;
+        do {
+            x+=y;
+            y++;
+        }
+        while (y<=85);
+        System.out.println(""+x);*/
+          int x=48;
+        while (x>35){
+
+            System.out.println(x -=3);
+                        
+        }
+    
+}
